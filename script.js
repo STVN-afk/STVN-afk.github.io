b2382921b176378c5db45a42169f426260b76f05
Steve <negativedegre3s@gmail.com>
2024-07-30 23:46:26 +0100

    Added a dropdown menu for Smaller Screens

--- script.js
+++ script.js
@@ -10,3 +10,29 @@ document.addEventListener('DOMContentLoaded', function () {
             link.classList.add('active');
     });
 });
+
+const sections = document.body.getElementsByTagName("section");
+
+function openMenu() {
+    document.getElementById("mobile_container").style.right="0%";
+    document.getElementById("mobile_menu").style.opacity="1";
+    hide(sections)
+    
+}
+
+function hide(sections2) {
+    for(let i = 0; i < sections2.length; i++) {
+        sections2[i].style.display="none";
+    }
+
+}function show(sections2) {
+    for(let i = 0; i < sections2.length; i++) {
+        sections2[i].style.display="block";
+    }
+}
+
+function closeMenu() {
+    document.getElementById("mobile_container").style.right="100%";
+    document.getElementById("mobile_menu").style.opacity="0";
+    show(sections)
+}
